6cb79c85099d4ae8238e967234030b7549e94c44
Daniel Martí <mvdan@mvdan.cc>
2014-11-14 21:41:03 +0100

    Remove unnecessary mContext stuff

--- App/src/main/java/cc/mvdan/template/MainActivity.java
+++ App/src/main/java/cc/mvdan/template/MainActivity.java
@@ -1,25 +1,16 @@
 package cc.mvdan.template;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 
 import cc.mvdan.template.R;
 
 public class MainActivity extends Activity {
 
-    private Context mContext;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mContext = this;
         setContentView(R.layout.main);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
     }
 
 }
